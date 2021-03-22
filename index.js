bc49cdaa4fa3d5e4343067deb3cba0641304b61f
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-22 11:37:58 +0100

    feat: create script's schell

---
+++ index.js
@@ -0,0 +1,11 @@
+const faker = require('faker');
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+async function bootstrap() {
+
+}
+
+bootstrap()
+    .catch(console.error);
